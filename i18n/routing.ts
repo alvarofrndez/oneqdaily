6afafce737d54b1de12c0dd7a06c783a930f684b
alvaro <alvarofernandezdomingo.trabajo@gmail.com>
2026-09-24 16:42:01 +0200

    fix(routing): redirectiong with locale

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,6 +1,7 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-    locales: ['en', 'es'],
-    defaultLocale: 'en'
+  locales: ['en', 'es'],
+  defaultLocale: 'en',
+  localePrefix: 'always'
 });
